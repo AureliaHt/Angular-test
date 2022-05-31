a7c735552c04752df30b66f40255ecb5388d6943
Aurélia Hemeret <aurelia.hemeret@gmail.com>
2022-05-31 17:48:35 +0200

    création des routes

--- ng-pokedex-angular/src/app/app-routing.module.ts
+++ ng-pokedex-angular/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DetailPokemonComponent } from './detail-pokemon/detail-pokemon.component';
+import { ListPokemonComponent } from './list-pokemon/list-pokemon.component';
 
-const routes: Routes = [];
+// routes vues du haut vers le bas
+// déclarer les routes spécifiques en premier et les globales en dernier
+const routes: Routes = [
+  { path: 'pokemons', component: ListPokemonComponent },
+  { path: 'pokemons/:id', component: DetailPokemonComponent },
+  { path: '', redirectTo: 'pokemons', pathMatch: 'full' } //patchMatch:full => équivaut à faire correspondre l'intégralité de l'url
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ ng-pokedex-angular/src/app/list-pokemon/list-pokemon.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-pokemon',
+  templateUrl: './list-pokemon.component.html'
+})
+export class ListPokemonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
