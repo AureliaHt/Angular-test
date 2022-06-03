2e5473fbcb700ae37afdfee6ad07eea7741bae78
Aurélia Hemeret <aurelia.hemeret@gmail.com>
2022-06-03 17:30:20 +0200

    fonction pour l'édition des types d'un pokémon

--- ng-pokedex-angular/src/app/pokemon/pokemon-form/pokemon-form.component.ts
+++ ng-pokedex-angular/src/app/pokemon/pokemon-form/pokemon-form.component.ts
@@ -34,6 +34,15 @@ export class PokemonFormComponent implements OnInit {
       }
   }
 
+  isTypesValid(type: string): boolean {
+    if(this.pokemon.types.length == 1 && this.hasType(type)) {
+        return false;
+    } if (this.pokemon.types.length >2 && !this.hasType(type)) {
+      return false;
+    }
+    return true;
+  }
+
   onSubmit() {
     console.log('Les changements ont été enregistré');
     this.router.navigate(['/pokemon', this.pokemon.id]);
