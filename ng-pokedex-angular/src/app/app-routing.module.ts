43a392b382f1146a8903e08796dab8f6426ee9ca
Aurélia Hemeret <aurelia.hemeret@gmail.com>
2022-06-03 12:30:03 +0200

    gestion des routes liées aux pokémons via le module, méthode forChild

--- ng-pokedex-angular/src/app/app-routing.module.ts
+++ ng-pokedex-angular/src/app/app-routing.module.ts
@@ -1,14 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DetailPokemonComponent } from './pokemon/detail-pokemon/detail-pokemon.component';
-import { ListPokemonComponent } from './pokemon/list-pokemon/list-pokemon.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 // routes lues du haut vers le bas
 // déclarer les routes spécifiques en premier et les globales en dernier
 const routes: Routes = [
-  { path: 'pokemons', component: ListPokemonComponent },
-  { path: 'pokemon/:id', component: DetailPokemonComponent },
   { path: '', redirectTo: 'pokemons', pathMatch: 'full' }, //patchMatch:full => équivaut à faire correspondre l'intégralité de l'url
   { path: '**', component: PageNotFoundComponent} // opérateur ** intercepte toutes les routes /!\ ordre de lecture des routes, bien mettre cet opérateur en dernier
 ];
--- ng-pokedex-angular/src/app/pokemon/pokemon.module.ts
+++ ng-pokedex-angular/src/app/pokemon/pokemon.module.ts
@@ -1,14 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DetailPokemonComponent } from './detail-pokemon/detail-pokemon.component';
+import { ListPokemonComponent } from './list-pokemon/list-pokemon.component';
+import { PokemonTypeColorPipe } from './pokemon-type-color.pipe';
+import { BorderCardDirective } from './border-card.directive';
+import { RouterModule, Routes } from '@angular/router';
 
+const pokemonRoutes: Routes =[
+  { path: 'pokemons', component: ListPokemonComponent },
+  { path: 'pokemon/:id', component: DetailPokemonComponent }
+];
 
 // Module pour centraliser tout ce qui concerne la gestion des pokémons afin de décharger le module racine
 // Angular = application modulaire, création de modules très utile sur de gros projets afin d'avoir une architecture moins chargée visuellement
 // On "branche" dans le module les différents composants, pipes, directives, etc.
 @NgModule({
-  declarations: [],
+  declarations: [
+    DetailPokemonComponent,
+    ListPokemonComponent,
+    PokemonTypeColorPipe,
+    BorderCardDirective
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(pokemonRoutes)
   ]
 })
 export class PokemonModule { }
