ae838fb4b2d4fd6242912d8ea82be10b87be1bad
Aurélia Hemeret <aurelia.hemeret@gmail.com>
2022-06-05 12:05:36 +0200

    ajout d'un nouveau service avec le module HttpClient et la librairieIn Memory Web Api pour simuler une Api

--- ng-pokedex-angular/src/app/app.module.ts
+++ ng-pokedex-angular/src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { PokemonModule } from './pokemon/pokemon.module';
+import { InMemoryDataService } from './in-memory-data.service';
 
 @NgModule({
   declarations: [
@@ -13,10 +16,14 @@ import { PokemonModule } from './pokemon/pokemon.module';
     PageNotFoundComponent
   ],
 
-  // attention à l'ordre de déclarations des moddules, bien prendre en compte l'ordre de lecture des routes
+  // attention à l'ordre de déclarations des modules, bien prendre en compte l'ordre de lecture des routes
+  // méthode .forRoot() pour retourner un module et ses dépendances
+  // option dataEncapsulation, par défaut à false pour insérer les données directement dans le body. True : encapsule le contenu  dans une data property
   imports: [
     BrowserModule,
     FormsModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, { dataEncapsulation:false }),
     PokemonModule,
     AppRoutingModule
   ],
